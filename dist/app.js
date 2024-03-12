605d2748e50165b812d866eabec7091b1a78a248
Rodfc773 <rodrigo.fc773@gmail.com>
2024-03-11 21:38:40 -0300

    Adicionando o Cors

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,8 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
 var _path = require('path');
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 
 var _userRoutes = require('./routes/userRoutes'); var _userRoutes2 = _interopRequireDefault(_userRoutes);
 var _tokenRoutes = require('./routes/tokenRoutes'); var _tokenRoutes2 = _interopRequireDefault(_tokenRoutes);
@@ -20,6 +22,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(_cors2.default.call(void 0, ));
+    this.app.use(_helmet2.default.call(void 0, ));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use(_express2.default.static(_path.resolve.call(void 0, __dirname, 'uploads')));
--- dist/controllers/TokenController.js
+++ dist/controllers/TokenController.js
@@ -18,7 +18,7 @@ class TokenController {
       process.env.TOKEN_SECRET,
       { expiresIn: process.env.TOKEN_EXPIRATION },
     );
-    return res.json({ token });
+    return res.json({ token, user: { nome: user.nome, id, email } });
   }
 }
 
